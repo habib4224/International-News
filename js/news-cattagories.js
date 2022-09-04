5ff58012c188f407352b0bf5e58520e36a9128f2
habib4224 <4224habib1@gmail.com>
2022-09-04 22:51:27 +0800

    news Catagories

--- js/news-cattagories.js
+++ js/news-cattagories.js
@@ -1,71 +1,26 @@
-
-function loadData() {
+let idPerNews = [];
+function newsCategoriesget() {
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
-        .then(data => display(data.data.news_category))
-
+        .then(data => displayCategories(data))
 }
-function display(newses) {
-    const newsCatagoriesContainer = document.getElementById('news-catagories');
-    for (const news of newses) {
-        // console.log(news)
+
+function displayCategories(data) {
+
+    const categoriesGet = data.data.news_category;
+    const categoriesContainer = document.getElementById('news-catagories');
+
+    for (const categoriesDatas of categoriesGet) {
+        idPerNews.push(categoriesDatas.category_id);
         const li = document.createElement('li');
-        // li.innerText = news.category_name;
-        // console.log(li)
+
         li.innerHTML = `
-        ${news.category_name}
+        <p id="${categoriesDatas.category_id}" onclick="newsCatagoriesClicked(${categoriesDatas.category_id})">${categoriesDatas.category_name}</p>
         `
-        newsCatagoriesContainer.appendChild(li);
-    }
-}
+        categoriesContainer.appendChild(li);
 
-function newsDataLoad() {
-    fetch('https://openapi.programming-hero.com/api/news/category/01')
-        .then(res => res.json())
-        .then(data => displayNews(data.data))
+    }
 
 }
-function displayNews(views) {
-    console.log(views)
-    const newsCatagoriesContainer = document.getElementById('news-display');
-    for (const view of views) {
-        console.log(view)
-        const divNews = document.createElement('div')
-        divNews.innerHTML = `
-        <div class=" border rounded-2 bg-light d-sm-flex  container  ">
-        <div><img src="${view.image_url}" class="img-fluid" alt="">
-        </div>
-        <div class="pt-3 ps-4">
-            <h4>${view.title}</h4>
-            <p>${view.details}
-            </p>
-            <div>
-                <div class="d-flex m-3">
-                    <div class="d-flex">
-                        <div class="w-25  h-25 m-3"> <img src="${view.author.img}" class="w-100 rounded-circle  h-100" alt=""></div>
-                        <div class=" m-3">
-                            <h6>${view.author.name}
-                            </h6>
-                            <p>${view.author.published_date}
-                            </p>
-                        </div>
-                    </div>
-                    <div class="d-flex m-3 ">
-                    <i class="fa-solid fa-eye p-2"></i>
-                        <h3 class="">${view.total_view}</h3>
-                    </div>
-                    <div class=" m-3">
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">
-                    details
-            </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-            `
-        newsCatagoriesContainer.appendChild(divNews)
-    }
-}
-newsDataLoad();
-loadData();
+
+newsCategoriesget();
